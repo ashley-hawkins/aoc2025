bb9f7db172a03f3971bee02cedaa6e4e4ab17a7e
Ashley Hawkins <awhawkins@proton.me>
2024-12-03 16:59:28 +0000

    Merge day 1

---
+++ src/day1.rs
@@ -0,0 +1,35 @@
+use std::{collections::HashMap, io};
+
+fn main() {
+    let mut left = Vec::<i32>::new();
+    let mut right = Vec::<i32>::new();
+    let mut right_count = HashMap::<i32, usize>::new();
+
+    let mut line: String = String::new();
+    while matches!(io::stdin().read_line(&mut line), Ok(n) if n > 0) {
+        let mut parts = line.split_ascii_whitespace();
+        left.push(parts.next().unwrap().parse().unwrap());
+        let right_elem = parts.next().unwrap().parse().unwrap();
+        right.push(right_elem);
+        *right_count.entry(right_elem).or_insert(0) += 1;
+        line.clear();
+    }
+
+    left.sort();
+    right.sort();
+
+    let (part1, part2) = left
+        .into_iter()
+        .zip(right)
+        .map(|(l, r)| {
+            (
+                l.abs_diff(r),
+                l as usize * right_count.get(&l).cloned().unwrap_or(0),
+            )
+        })
+        .fold((0, 0), |(acc_diff, acc_sum), (diff, sum)| {
+            (acc_diff + diff, acc_sum + sum)
+        });
+
+    println!("Part 1: {}, Part 2: {}", part1, part2);
+}
