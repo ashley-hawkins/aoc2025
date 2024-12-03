affdf6f05a35ad5d2c835784e8b87fcf335ac496
Ashley Hawkins <awhawkins@proton.me>
2024-12-03 21:26:48 +0000

    Day 3 Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -2,19 +2,44 @@ use regex::Regex;
 use util::stdin_lines;
 
 fn main() {
-    let mul_pattern = Regex::new(r#"mul\((\d+),(\d+)\)"#).unwrap();
-    let total: i32 = stdin_lines()
-        .map(|line| -> i32 {
-            mul_pattern
-                .captures_iter(&line)
-                .map(|captures| {
-                    let left: i32 = captures.get(1).unwrap().as_str().parse().unwrap();
-                    let right: i32 = captures.get(2).unwrap().as_str().parse().unwrap();
-                    left * right
-                })
-                .sum()
-        })
-        .sum();
+    let instruction_pattern = Regex::new(r#"do\(\)|don't\(\)|mul\((\d+),(\d+)\)"#).unwrap();
 
-    println!("{}", total);
+    let mut active = true;
+    let (part1, part2) = stdin_lines()
+        .map(|line| -> (i32, i32) {
+            instruction_pattern
+                .captures_iter(&line)
+                .filter_map(|captures| {
+                    let main_group = captures.get(0).unwrap();
+
+                    match main_group.as_str() {
+                        "do()" => {
+                            active = true;
+                            None
+                        }
+                        "don't()" => {
+                            active = false;
+                            None
+                        }
+                        _ => {
+                            let left: i32 = captures.get(1).unwrap().as_str().parse().unwrap();
+                            let right: i32 = captures.get(2).unwrap().as_str().parse().unwrap();
+                            let res = left * right;
+                            if !active {
+                                Some((res, 0))
+                            } else {
+                                Some((res, res))
+                            }
+                        }
+                    }
+                })
+                .fold((0, 0), |(acc1, acc2), (val1, val2)| {
+                    (acc1 + val1, acc2 + val2)
+                })
+        })
+        .fold((0, 0), |(acc1, acc2), (val1, val2)| {
+            (acc1 + val1, acc2 + val2)
+        });
+
+    println!("Part 1: {part1} / Part 2: {part2}");
 }
