e72447638ad12d238148aa267136f6221a85eb9d
Ashley Hawkins <awhawkins@proton.me>
2024-12-05 17:13:16 +0000

    Add util functions for file input

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,5 +1,16 @@
-use std::io::{self, BufRead};
+use std::{
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
 
 pub fn stdin_lines() -> impl Iterator<Item = String> {
-    io::stdin().lock().lines().map(Result::unwrap)
+    stream_lines(io::stdin().lock())
+}
+
+pub fn file_lines(p: impl AsRef<Path>) -> impl Iterator<Item = String> {
+    stream_lines(BufReader::new(std::fs::File::open(p).unwrap()))
+}
+
+pub fn stream_lines(stream: impl BufRead) -> impl Iterator<Item = String> {
+    stream.lines().map(Result::unwrap)
 }
