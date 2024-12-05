5577f55beaaa7c1b453a9c69f37fab2f50fd4586
Ashley Hawkins <awhawkins@proton.me>
2024-12-05 11:38:17 +0000

    Day 5 Part 1

---
+++ src/day5.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+
+use util::stdin_lines;
+
+fn main() {
+    let mut lines = stdin_lines();
+
+    let rules = {
+        let rules_iter = lines.by_ref().take_while(|line| !line.is_empty());
+        rules_iter
+            .map(|line| {
+                let mut parts = line.split("|");
+                let part1: i32 = parts.next().unwrap().parse().unwrap();
+                let part2: i32 = parts.next().unwrap().parse().unwrap();
+
+                (part1, part2)
+            })
+            .collect::<HashSet<_>>()
+    };
+
+    let results: i32 = lines
+        .by_ref()
+        .map(|line| {
+            let mut parts = line
+                .split(",")
+                .map(|element| element.parse().unwrap())
+                .collect::<Vec<i32>>();
+
+            for i in 0..parts.len() {
+                for j in i + 1..parts.len() {
+                    if rules.contains(&(parts[j], parts[i])) {
+                        return 0;
+                    }
+                }
+            }
+            parts[parts.len() / 2]
+        })
+        .sum();
+
+    println!("Result: {}", results);
+}
