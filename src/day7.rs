b443e6aeea53ed6f2f1421c1c2165b27abc6027a
Ashley Hawkins <awhawkins@proton.me>
2024-12-07 18:09:30 +0000

    More efficient concat for day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,15 +1,19 @@
-fn solve(lines: impl Iterator<Item = String>) -> (i64, i64) {
+fn concat(a: u64, b: u64) -> u64 {
+    a * 10u64.pow(b.checked_ilog10().unwrap_or(0) + 1) + b
+}
+
+fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
     let mut part1 = 0;
     let mut part2 = 0;
 
     'outer: for line in lines {
         let mut parts = line.split(":");
-        let result: i64 = parts
+        let result: u64 = parts
             .next()
             .unwrap()
             .parse()
             .expect(format!("Failed to parse {}", line).as_str());
-        let operands: Vec<i64> = parts
+        let operands: Vec<u64> = parts
             .next()
             .unwrap()
             .split_whitespace()
@@ -41,9 +45,7 @@ fn solve(lines: impl Iterator<Item = String>) -> (i64, i64) {
                     0 => sum *= operands[j + 1],
                     1 => sum += operands[j + 1],
                     2 => {
-                        sum = (sum.to_string() + &operands[j + 1].to_string())
-                            .parse()
-                            .unwrap()
+                        sum = concat(sum, operands[j + 1]);
                     }
                     _ => unreachable!(),
                 }
@@ -64,4 +66,21 @@ fn main() {
 
     println!("Part 1: {part1}");
     println!("Part 2: {part2}");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_concat() {
+        for i in 0..1000 {
+            for j in 0..1000 {
+                assert_eq!(
+                    concat(i, j),
+                    (i.to_string() + &j.to_string()).parse().unwrap()
+                );
+            }
+        }
+    }
 }
