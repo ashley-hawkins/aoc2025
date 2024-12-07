1e3401e4e3646e91380f49ee8264a2a710a51747
Ashley Hawkins <awhawkins@proton.me>
2024-12-07 16:50:09 +0000

    Day 7 Part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,35 +1,62 @@
 fn solve(lines: impl Iterator<Item = String>) -> (i64, i64) {
-    let part1 = lines
-        .filter_map(|line| {
-            let mut parts = line.split(":");
-            let result: i64 = parts.next().unwrap().parse().expect(format!("Failed to parse {}", line).as_str());
-            let operands: Vec<i64> = parts
-                .next()
-                .unwrap()
-                .split_whitespace()
-                .map(|operand| operand.parse().unwrap())
-                .collect();
+    let mut part1 = 0;
+    let mut part2 = 0;
 
-            for i in 0..(1 << (operands.len() - 1)) {
-                let mut sum = operands[0];
-                for j in 0..operands.len() - 1 {
-                    if i & (1 << j) != 0 {
-                        sum += operands[j + 1];
-                    } else {
-                        sum *= operands[j + 1];
-                    }
-                }
+    'outer: for line in lines {
+        let mut parts = line.split(":");
+        let result: i64 = parts
+            .next()
+            .unwrap()
+            .parse()
+            .expect(format!("Failed to parse {}", line).as_str());
+        let operands: Vec<i64> = parts
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .map(|operand| operand.parse().unwrap())
+            .collect();
 
-                if sum == result {
-                    return Some(result);
+        for i in 0..(1 << (operands.len() - 1)) {
+            let mut sum = operands[0];
+            for j in 0..operands.len() - 1 {
+                if i & (1 << j) != 0 {
+                    sum += operands[j + 1];
+                } else {
+                    sum *= operands[j + 1];
                 }
             }
 
-            None
-        })
-        .sum();
+            if sum == result {
+                part1 += result;
+                part2 += result;
+                continue 'outer;
+            }
+        }
 
-    (part1, 0)
+        for i in 0..(3i64.pow((operands.len() - 1) as u32)) {
+            let mut sum = operands[0];
+            for j in 0..operands.len() - 1 {
+                let op = (i / 3i64.pow(j as u32)) % 3;
+                match op {
+                    0 => sum *= operands[j + 1],
+                    1 => sum += operands[j + 1],
+                    2 => {
+                        sum = (sum.to_string() + &operands[j + 1].to_string())
+                            .parse()
+                            .unwrap()
+                    }
+                    _ => unreachable!(),
+                }
+            }
+
+            if sum == result {
+                part2 += result;
+                continue 'outer;
+            }
+        }
+    }
+
+    (part1, part2)
 }
 
 fn main() {
