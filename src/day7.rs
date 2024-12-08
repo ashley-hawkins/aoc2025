3c1f6c29d7bdd70a68be0ae7cdcbbd90b97d30d2
Ashley Hawkins <awhawkins@proton.me>
2024-12-08 10:38:34 +0000

    Day 7: experiment with parallelisation using rayon

--- src/day7.rs
+++ src/day7.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use rayon::prelude::*;
 
 #[derive(Clone, Copy)]
 enum Operator {
@@ -11,68 +12,71 @@ fn concat(a: u64, b: u64) -> u64 {
     a * 10u64.pow(b.checked_ilog10().unwrap_or(0) + 1) + b
 }
 
-fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
-    let mut part1 = 0;
-    let mut part2 = 0;
+fn solve(mut lines: impl Iterator<Item = String>) -> (u64, u64) {
+    let (part1, part2) = lines
+        .collect_vec()
+        .par_iter()
+        .map(|line| {
+            let mut parts = line.split(":");
+            let result: u64 = parts
+                .next()
+                .unwrap()
+                .parse()
+                .unwrap_or_else(|_| panic!("Failed to parse {}", line));
 
-    'outer: for line in lines {
-        let mut parts = line.split(":");
-        let result: u64 = parts
-            .next()
-            .unwrap()
-            .parse()
-            .unwrap_or_else(|_| panic!("Failed to parse {}", line));
+            let operands: Vec<u64> = parts
+                .next()
+                .unwrap()
+                .split_whitespace()
+                .map(|operand| operand.parse().unwrap())
+                .collect();
 
-        let operands: Vec<u64> = parts
-            .next()
-            .unwrap()
-            .split_whitespace()
-            .map(|operand| operand.parse().unwrap())
-            .collect();
+            let starting_point = operands[0];
 
-        let starting_point = operands[0];
-
-        for ops in std::iter::repeat([Operator::Add, Operator::Mul])
-            .take(operands.len() - 1)
-            .multi_cartesian_product()
-        {
-            let mut sum = starting_point;
-            for (op, operand) in ops.iter().zip(operands.iter().skip(1)) {
-                match op {
-                    Operator::Mul => sum *= operand,
-                    Operator::Add => sum += operand,
-                    _ => unreachable!(),
-                }
-            }
-
-            if sum == result {
-                part1 += result;
-                part2 += result;
-                continue 'outer;
-            }
-        }
-
-        for ops in std::iter::repeat([Operator::Add, Operator::Mul, Operator::Concat])
-            .take(operands.len() - 1)
-            .multi_cartesian_product()
-        {
-            let mut sum = starting_point;
-            for (op, operand) in ops.iter().zip(operands.iter().skip(1)) {
-                match op {
-                    Operator::Mul => sum *= operand,
-                    Operator::Add => sum += operand,
-                    Operator::Concat => {
-                        sum = concat(sum, *operand);
+            for ops in std::iter::repeat([Operator::Add, Operator::Mul])
+                .take(operands.len() - 1)
+                .multi_cartesian_product()
+            {
+                let mut sum = starting_point;
+                for (op, operand) in ops.iter().zip(operands.iter().skip(1)) {
+                    match op {
+                        Operator::Mul => sum *= operand,
+                        Operator::Add => sum += operand,
+                        _ => unreachable!(),
                     }
                 }
+
+                if sum == result {
+                    return (result, result);
+                }
             }
 
-            if sum == result {
-                part2 += result;
-                continue 'outer;
+            for ops in std::iter::repeat([Operator::Add, Operator::Mul, Operator::Concat])
+                .take(operands.len() - 1)
+                .multi_cartesian_product()
+            {
+                let mut sum = starting_point;
+                for (op, operand) in ops.iter().zip(operands.iter().skip(1)) {
+                    match op {
+                        Operator::Mul => sum *= operand,
+                        Operator::Add => sum += operand,
+                        Operator::Concat => {
+                            sum = concat(sum, *operand);
+                        }
+                    }
+                }
+
+                if sum == result {
+                    return (0, result);
+                }
             }
-        }
-    }
+
+            (0, 0)
+        })
+        .reduce(
+            || (0, 0),
+            |(part1, part2), (part1_, part2_)| (part1 + part1_, part2 + part2_),
+        );
 
     (part1, part2)
 }
