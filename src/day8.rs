7d6c60e62a7d1e78bd6739d75ed1ec610c00e6a5
Ashley Hawkins <awhawkins@proton.me>
2024-12-08 16:57:28 +0000

    Day 8 Part 2

--- src/day8.rs
+++ src/day8.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use itertools::Itertools;
 use multimap::MultiMap;
 
-fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
+fn solve(lines: impl Iterator<Item = String>) -> (usize, usize) {
     let mut map = MultiMap::new();
 
     let mut width: i64 = 0;
@@ -24,11 +24,13 @@ fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
     }
 
     let mut antinodes: HashSet<(i64, i64)> = HashSet::new();
-    for (frequency, positions) in map.iter_all() {
+    let mut antinodes2: HashSet<(i64, i64)> = HashSet::new();
+    for (_, positions) in map.iter_all() {
         for pair in positions.iter().combinations(2) {
             let (l, r) = <[_; 2]>::try_from(pair).unwrap().into();
             let difference1 = (2 * l.0 - r.0, 2 * l.1 - r.1);
             let difference2 = (2 * r.0 - l.0, 2 * r.1 - l.1);
+            let difference3 = (l.0 - r.0, l.1 - r.1);
 
             let valid_x = 0..width;
             let valid_y = 0..height;
@@ -40,10 +42,30 @@ fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
             if valid_x.contains(&difference2.0) && valid_y.contains(&difference2.1) {
                 antinodes.insert(difference2);
             }
+
+            antinodes2.insert(*l);
+            antinodes2.insert(*r);
+            let mut forward_positions = *r;
+            while valid_x.contains(&forward_positions.0) && valid_y.contains(&forward_positions.1) {
+                antinodes2.insert(forward_positions);
+                forward_positions = (
+                    forward_positions.0 + difference3.0,
+                    forward_positions.1 + difference3.1,
+                );
+            }
+
+            let mut back_positions = *r;
+            while valid_x.contains(&back_positions.0) && valid_y.contains(&back_positions.1) {
+                antinodes2.insert(back_positions);
+                back_positions = (
+                    back_positions.0 - difference3.0,
+                    back_positions.1 - difference3.1,
+                );
+            }
         }
     }
 
-    (antinodes.len() as u64, 0)
+    (antinodes.len(), antinodes2.len())
 }
 
 fn main() {
