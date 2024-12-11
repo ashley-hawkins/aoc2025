c31b4e1b7bc40ac3c6dd6626e02af7f03b580f52
Ashley Hawkins <awhawkins@proton.me>
2024-12-11 10:50:30 +0000

    Day 11 Part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,6 @@
+use std::{collections::HashMap, num::NonZero};
+
+use rayon::iter::{IntoParallelIterator, IntoParallelRefMutIterator, ParallelIterator};
 use util::stdin_lines;
 
 fn transform(input: i64) -> (Option<i64>, i64) {
@@ -44,30 +47,77 @@ fn solve(mut lines: impl Iterator<Item = String>) -> (i64, i64) {
 
 */
 
-fn transform_vec(input: &mut Vec<i64>) {
-    for i in 0..input.len() {
-        let (l, r) = transform(input[i]);
-        input[i] = r;
-
+fn transform_vec<'a>(input: HashMap<i64, u64>) -> HashMap<i64, u64> {
+    let mapper = |(n, count)| {
+        let (l, r) = transform(n);
         if let Some(l) = l {
-            input.push(l);
+            vec![(l, count), (r, count)]
+        } else {
+            vec![(r, count)]
         }
-    }
+    };
+    input
+        .into_par_iter()
+        .flat_map(mapper)
+        .flat_map(mapper)
+        .flat_map(mapper)
+        .flat_map(mapper)
+        .flat_map(mapper)
+        .fold(
+            || HashMap::new(),
+            |mut acc, (n, count)| {
+                *acc.entry(n).or_insert(0) += count;
+                acc
+            },
+        )
+        .reduce(
+            || HashMap::new(),
+            |mut acc, rhs| {
+                for (n, count) in rhs {
+                    *acc.entry(n).or_insert(0) += count;
+                }
+                acc
+            },
+        )
 }
 
-fn solve(mut lines: impl Iterator<Item = String>) -> (usize, u64) {
+fn solve(mut lines: impl Iterator<Item = String>) -> (u64, u64) {
     let mut the_numbers = lines
         .next()
         .unwrap()
         .split_whitespace()
         .map(|s| s.parse().unwrap())
-        .collect::<Vec<i64>>();
+        .fold(HashMap::new(), |mut acc, n| {
+            *acc.entry(n).or_insert(0) += 1u64;
+            acc
+        });
 
-    for i in 0..25 {
-        transform_vec(&mut the_numbers);
+    let mut res = the_numbers;
+
+    for i in 0..5 {
+        res = transform_vec(res);
     }
 
-    (the_numbers.len(), 0)
+    let part1 = res.iter().fold(0, |acc, (_, &v)| acc + v);
+
+    // let mut count_the_numbers = the_numbers.iter().fold(HashMap::new(), |mut acc, n| {
+    //     *acc.entry(n).or_insert(0) += 1u32;
+    //     acc
+    // });
+
+    for i in 0..10 {
+        res = transform_vec(res);
+    }
+
+    let part2 = res.iter().fold(0, |acc, (_, &v)| acc + v);
+    // let max_duplicate = count_the_numbers.iter().max_by_key(|(_, &v)| v).unwrap().0;
+    // println!("Max duplicate: {:?}", max_duplicate);
+
+    // println!("{:?}", the_numbers.len());
+
+    println!("{:?}", res.len());
+
+    (part1, part2)
 }
 
 fn main() {
