b75889321a4390fe2f9090fc09a3d25bdf391ee6
Ashley Hawkins <awhawkins@proton.me>
2025-12-03 15:40:39 +0000

    Day 1 Part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -3,6 +3,7 @@ use util::stdin_lines;
 fn solve(lines: impl Iterator<Item = String>) -> (u32, u32) {
     let mut dial: i32 = 50;
     let mut count: u32 = 0;
+    let mut count2: u32 = 0;
 
     for line in lines {
         let mut chars = line.chars();
@@ -10,14 +11,22 @@ fn solve(lines: impl Iterator<Item = String>) -> (u32, u32) {
 
         let amount: i32 = direction * chars.as_str().parse::<i32>().unwrap();
 
-        dial = (dial + amount).rem_euclid(100);
+        let dial_was_zero = dial == 0;
+        dial += amount;
+
+        count2 += (dial / 100).unsigned_abs();
+        if dial <= 0 && !dial_was_zero {
+            count2 += 1;
+        }
+
+        dial = dial.rem_euclid(100);
 
         if dial == 0 {
             count += 1;
         }
     }
 
-    (count, 0)
+    (count, count2)
 }
 
 fn main() {
