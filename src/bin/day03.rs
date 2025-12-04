7454e07ab2daea231b023e0f9ab7dbd10ac6abee
Ashley Hawkins <awhawkins@proton.me>
2025-12-04 22:54:36 +0000

    Day 3 Part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,28 +1,51 @@
 use util::stdin_lines;
 
-fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
-    let mut dial: i32 = 50;
-    let mut count: u64 = 0;
+fn max_joltage_with_digits(joltages: &[u8], digits: usize) -> u64 {
+    let mut best_digits: Vec<usize> = Vec::with_capacity(digits);
 
-    for line in lines {
-        // guaranteed to be ascii
-        let chars = line.as_bytes();
+    for i in 0..best_digits.capacity() {
+        let start_idx = if let Some(&x) = best_digits.last() {
+            x + 1
+        } else {
+            0
+        };
 
-        let mut max_joltage_index: usize = 0;
-        for i in 0..chars.len() - 1 {
-            let joltage = chars[i] - b'0';
-
-            if joltage > chars[max_joltage_index] - b'0' {
-                max_joltage_index = i;
-            }
-        }
-
-        let max_joltage = ((chars[max_joltage_index] - b'0') * 10)
-            + chars[max_joltage_index + 1..].iter().map(|x| x - b'0').max().unwrap();
-        count += max_joltage as u64;
+        let max_joltage_index = joltages
+            [start_idx..joltages.len() - best_digits.capacity() + i + 1]
+            .iter()
+            .enumerate()
+            .rev()
+            .max_by_key(|(_, &j)| j)
+            .unwrap()
+            .0
+            + start_idx;
+        best_digits.push(max_joltage_index);
     }
 
-    (count, 0)
+    let max_joltage = best_digits
+        .iter()
+        .fold(0u64, |acc, &idx| acc * 10 + joltages[idx] as u64);
+
+    max_joltage
+}
+
+fn solve(lines: impl Iterator<Item = String>) -> (u64, u64) {
+    let mut count: u64 = 0;
+    let mut count2: u64 = 0;
+
+    for line in lines {
+        let joltages = line
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect::<Vec<_>>();
+
+        let part1 = max_joltage_with_digits(&joltages, 2);
+        count += part1;
+        let part2 = max_joltage_with_digits(&joltages, 12);
+        count2 += part2;
+    }
+
+    (count, count2)
 }
 
 fn main() {
