74ed0c8506d8d24ab563b13cef81f84cd8eba4c9
Ashley Hawkins <awhawkins@proton.me>
2025-12-05 01:49:52 +0000

    Day 4 Part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,32 +1,49 @@
 use util::{lines_to_grid, stdin_lines};
 
-fn solve(grid: ndarray::Array2<u8>) -> (u64, u64) {
+fn solve(mut grid: ndarray::Array2<u8>) -> (u64, u64) {
     let mut part1 = 0;
-    for row in 0..grid.dim().0 {
-        for col in 0..grid.dim().1 {
-            if grid[(row, col)] != b'@' {
-                continue;
-            }
+    let mut part2 = 0;
+    for i in 0usize.. {
+        let mut this_pass = 0;
+        let grid_snapshot = grid.clone();
 
-            let mut count = 0;
-            for i in -1..=1 {
-                for j in -1..=1 {
-                    if let Some(new_row) = (row as isize).checked_add(i) && let Some(new_col) = (col as isize).checked_add(j) {
-                        let new_row = new_row as usize;
-                        let new_col = new_col as usize;
-                        if grid.get((new_row, new_col)) == Some(&b'@') {
-                            count += 1;
+        for row in 0..grid.dim().0 {
+            for col in 0..grid.dim().1 {
+                if grid_snapshot[(row, col)] != b'@' {
+                    continue;
+                }
+
+                let mut count = 0;
+                for i in -1..=1 {
+                    for j in -1..=1 {
+                        if let Some(new_row) = (row as isize).checked_add(i)
+                            && let Some(new_col) = (col as isize).checked_add(j)
+                        {
+                            let new_row = new_row as usize;
+                            let new_col = new_col as usize;
+                            if grid_snapshot.get((new_row, new_col)) == Some(&b'@') {
+                                count += 1;
+                            }
                         }
                     }
                 }
-            }
 
-            if count <= 4 {
-                part1 += 1;
+                if count <= 4 {
+                    this_pass += 1;
+                    grid[(row, col)] = b'.';
+                }
             }
         }
+        if this_pass == 0 {
+            break;
+        }
+
+        if i == 0 {
+            part1 = this_pass;
+        }
+        part2 += this_pass;
     }
-    (part1, 0)
+    (part1, part2)
 }
 
 fn main() {
